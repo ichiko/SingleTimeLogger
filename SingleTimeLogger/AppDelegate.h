d56ba581437d3d4ccae970f2456a1db700b683fb
ichiko <ichigo_sky@hotmail.com>
2013-12-31 21:04:34 +0900

    Initial Commit

---
+++ SingleTimeLogger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SingleTimeLogger
+//
+//  Created by ichiko on 2013/12/31.
+//  Copyright (c) 2013年 ichiko_revjune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SingleTimeLoggerTests/SingleTimeLoggerTests.m
@@ -0,0 +1,34 @@
+//
+//  SingleTimeLoggerTests.m
+//  SingleTimeLoggerTests
+//
+//  Created by ichiko on 2013/12/31.
+//  Copyright (c) 2013年 ichiko_revjune. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SingleTimeLoggerTests : XCTestCase
+
+@end
+
+@implementation SingleTimeLoggerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
